39c394534f78d85fd94ec6dec909d1a31c9be2b9
Mateus Santos <116230356+Mateus110@users.noreply.github.com>
2022-11-16 23:32:25 +0000

    estamos aprendendo a buscar dados em um banco de dados

---
+++ aula 2-2022-26-10c.py
@@ -0,0 +1,15 @@
+#exemplo de laço
+
+#se eu quisesse exibir numeros de 1 a 10
+print("aqui é a maneira rustica")
+print("1")
+print("2")
+print("3")
+print("4")
+print("5")
+print("6")
+print("7")
+print("8")
+print("9")
+print("10")
+
